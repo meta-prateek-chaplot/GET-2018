b6e8e6eaa9307ee50073324e7c3678dbe2aaaef9
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-02-02 08:00:49 +0530

    OOP Session-1 Assignments

---
+++ OOP/Session-1/Span.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Prateek
+ * Span is a HTML tag
+ * Span is type of Composite Element
+ */
+public class Span extends CompositeElement{
+	
+	// Constructor to initialize the values
+	Span(String id, String className){
+		
+		// calling parent class for initialization
+		super(id , className);
+	}
+}
