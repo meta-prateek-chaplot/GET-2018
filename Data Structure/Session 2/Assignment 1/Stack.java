6f5c4ddbccae0fa25f740ed2596cf39f26fb7dce
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-03-19 15:03:44 +0530

    Data Structure Session-2 Assignment-1

---
+++ Data Structure/Session 2/Assignment 1/Stack.java
@@ -0,0 +1,35 @@
+package com.metacube;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * 
+ * @author Prateek Chaplot
+ *
+ */
+public class Stack {
+	private List<Object> obj = new LinkedList<Object>();	// stack implemented using list
+	
+	/**
+	 * push functionality
+	 * @param obj
+	 */
+	public void push(Object obj) {
+		this.obj.add(obj);
+	}
+	
+	/**
+	 * pop functionality
+	 */
+	public void pop() {
+		obj.remove(obj.size()-1);
+	}
+	
+	/**
+	 * for testing purpose
+	 * @return stack of objects
+	 */
+	public Object[] getList() {
+		return obj.toArray();
+	}
+}
